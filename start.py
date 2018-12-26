e9a18da9661c66ef8e55b4c4d448d472a06cb144
Bogu <git@synapse.com.pl>
2018-12-27 00:06:14 +0100

    Video capture sample class

--- start.py
+++ start.py
@@ -1,44 +1,74 @@
 import cv2 as cv
 import argparse
 
-parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-parser.add_argument('--camera', help='Camera devide number.', type=int, default=0)
-args = parser.parse_args()
+class Kamera(object):
 
-camera_device = args.camera
+    def __init__(self):
+        self.parser=None
+        self.args=None
+        self.camera_device=None
+        self.cap=None
+        self.argument()
+        self.create_capture()
 
-#-- 2. Read the video stream
+    def __del__(self):
+        if not self.cap is None:
+            # When everything done, release the video capture object
+            self.cap.release()
+        # Closes all the frames
+        cv.destroyAllWindows()
 
-cap = cv.VideoCapture(camera_device)
+    def argument(self):
+        self.parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
+        self.parser.add_argument('--face_cascade', help='Path to face cascade.',
+                            default='data/haarcascades/haarcascade_frontalface_alt.xml')
+        self.parser.add_argument('--eyes_cascade', help='Path to eyes cascade.',
+                            default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+        self.parser.add_argument('--camera', help='Camera devide number.', type=int, default=0)
+        self.args = self.parser.parse_args()
+        if self.args is None:
+            print("ArgumentParser error..")
+            exit(0)
+        else:
+            self.camera_device = self.args.camera
 
-if not cap.isOpened:
-    print('--(!)Error opening video capture')
-    exit(0)
-
-while True:
-    ret, frame = cap.read()
-    if frame is None:
-        print('-- (!) No captured frame -- Break!')
-        break
-    else:
-        # Display the resulting frame
-        cv.imshow('Frame', frame)
-        #print('-- jest ok..')
-
-    #detectAndDisplay(frame)
-
-    if cv.waitKey(10) == 27:
-        break
-
-
-
-print( "Start z opencv...", cv.__version__)
+    def create_capture(self):
+        # -- 2. Read the video stream
+        self.cap = cv.VideoCapture(self.camera_device)
+        if not self.cap.isOpened:
+            print('--(!)Error opening video capture')
+            exit(0)
 
 
-# When everything done, release the video capture object
-cap.release()
+    def run(self):
+        while True:
+            if self.cap is None:
+                print('-- (!) No captured frame -- Break!')
+            else:
+                ret, frame = self.cap.read()
+                if frame is None:
+                    print('-- (!) No captured frame -- Break!')
+                    break
+                else:
+                    # Display the resulting frame
+                    cv.imshow('Frame', frame)
+                    #print('-- jest ok..')
+                    #detectAndDisplay(frame)
+            if cv.waitKey(10) == 27:
+                break
 
-# Closes all the frames
-cv.destroyAllWindows()
+
+if __name__=='__main__':
+    print("Start z opencv...", cv.__version__)
+
+    kamera=Kamera()
+    kamera.run()
+
+
+
+
+
+
+
+
+
